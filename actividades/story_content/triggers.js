6b6a3d569d0158b2d8740a4175c7b2fa020ca8f3
maoSantaella <mao.santaella@gmail.com>
2024-06-22 04:44:13 +0000

    new dist

---
+++ actividades/story_content/triggers.js
@@ -0,0 +1,18 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6K1R1AjcNTG":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
